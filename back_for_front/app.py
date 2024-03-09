81953a10affbd11918030667dac2d6ededd351ab
marienov <senya2104@gmail.com>
2024-03-09 19:15:56 +0300

    fixed

--- back_for_front/app.py
+++ back_for_front/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 import psycopg2
+import requests
 
 
 app = Flask(__name__)
 
 try:    
     conn = psycopg2.connect(dbname='mars', user='postgres', password='111111', host='localhost', port = '5432')
+    conn.autocommit = True
     print('Connection established')
 
 except psycopg2.Error as e:
@@ -16,30 +18,27 @@ except psycopg2.Error as e:
 def post_func():    
 
     req = request
-    print('-------')
-    print(req)
     for file_name in req.files.keys():
         file = req.files[file_name]
-        #print(file_path)
 
 
+
+    file_name += '_' + str(hash(req.form['text'])) 
     with open(file_name, 'wb') as f:
         f.write(file.read())
-    print(req.form['text'])
-    print(file_name)
-    print(req.form['name'])
     # Берем данные из запроса (отчет сначала в переменную, потом в бд, а файл сразу в папку) 
     # Берем переменную отчета и пишем в бд: conn.exec("INSERT запрос")
     try:
         cur = conn.cursor()
         res = cur.execute("INSERT INTO public.report(report_text, path, name) VALUES (%s, %s, %s);", 
         (req.form['text'], file_name, req.form['name']))
-        print(res)
         cur.close()
     except:
         print('Can`t insert into database')
 
-    conn.close()
+    # отправляем другому серверу информационное сообщение
+    response = requests.post("http://127.0.0.1:4000/inform", data={'event':'new_report'})
+
     return 'OK'
 
 
