f2cbac76c8aec4021e9c862a32115e3a42ef09bf
angelova <angelova.03@mail.ru>
2024-03-10 20:12:22 +0300

    commit one more

--- flutter_application/lib/src/home/home.dart
+++ flutter_application/lib/src/home/home.dart
@@ -4,8 +4,6 @@ import '../settings/settings_view.dart';
 import 'package:dio/dio.dart';
 // import 'dio';
 
-
-
 class ReportPage extends StatelessWidget {
   static const routeName = '/';
 
@@ -27,9 +25,6 @@ class ReportPage extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
               Navigator.restorablePushNamed(context, SettingsView.routeName);
             },
           ),
